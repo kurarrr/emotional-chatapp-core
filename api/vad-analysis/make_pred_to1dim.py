1838e327ba590481d83f501b5fba3b4822fd3398
Yonekura <reverse.engau@gmail.com>
2018-09-06 01:55:57 +0900

    import os

--- api/vad-analysis/make_pred_to1dim.py
+++ api/vad-analysis/make_pred_to1dim.py
@@ -19,7 +19,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 from nltk.corpus import wordnet
-import json
+import json,os,sys
 
 
 print('word2vec loading ...')
