38c7b969f4d1eba4265a08d3befa5b7b4a74683c
Yonekura <reverse.engau@gmail.com>
2018-12-06 23:30:28 +0900

    update transform

--- api/transform.py
+++ api/transform.py
@@ -3,4 +3,4 @@ from gensim.models import KeyedVectors
 
 from gensim.scripts.glove2word2vec import glove2word2vec
 # transform : glove -> tmp
-glove2word2vec('./analysis/stanford_glove.txt', './analysis/stanford_w2v.txt')
+glove2word2vec('./analysis/glove.840B.300d.txt', './analysis/stanford_w2v.txt')
