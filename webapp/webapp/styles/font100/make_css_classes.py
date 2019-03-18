7e3a345e76fd36c6ad875096efb034493abbb6a1
Yonekura <reverse.engau@gmail.com>
2019-03-18 20:03:16 +0900

    webapp is not submodule

---
+++ webapp/webapp/styles/font100/make_css_classes.py
@@ -0,0 +1,19 @@
+# fontのあるディレクトリで実行するとcssを生成する
+
+import sys,os
+
+files = os.listdir("./")
+css = ""
+
+for f in files:
+    t = """
+    .font-{0}{{
+        font-family : '{1}';
+    }}
+    """.format(f.replace('.','-'),f)
+    # class名に.は使えないのでreplace
+
+    css = css + t
+
+with open('./font100-effect.css','w') as fp:
+    fp.write(css)
